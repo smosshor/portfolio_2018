d41df0e2c3502a94029fec4895b19964a3e57693
smosshor <smosshorwitz@gmail.com>
2018-01-10 08:34:46 -0500

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -8,14 +8,13 @@ $(document).ready(function () {
     // Click to show portfolio details, hide when 'X' is clicked
     $("#card1").on('click', function () {
         $("#item1").load("animalcop.html");
-        console.log('DONE');
-
         $("#item1").toggleClass("d-none");
         $("#item1").addClass("animated fadeInUp wow");
     });
 
-    $("#item1 i").on('click', function () {
+    $("#item1x").on('click', function () {
         $("#item1").toggleClass("d-none");
+
     });
 
 });
