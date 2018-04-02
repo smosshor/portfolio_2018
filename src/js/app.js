7ec30f6cd14334b0f6028fab695d0d77e64d8a7f
Sarah MH <shorwitz@gofundme.com>
2018-04-01 21:15:29 -0400

    /

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {});
 // Add page load 
 $(window).on('load', function () { // makes sure the whole site is loaded 
     $('#status').fadeOut(); // will first fade out the loading animation 
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('#preloader-1').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
     $('body').delay(350).css({
         'overflow': 'visible'
     });
